86b5144d49d9b04b0402129c861c1f0d963e635f
CasHensen <102985001+CasHensen@users.noreply.github.com>
2022-10-27 10:02:34 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,7 @@ import string
 def get_preprocessed_data(names):
     
     output = " ".join([open(name,encoding = 'utf8').read() for name in names]).replace("\n",
-                                                                                  ". ").replace(" uh", " ").lower()
+                                                                                  ". ").replace(" uh ", " ").replace("[Music]", "").lower()
     
     output = output.translate(str.maketrans('', '', string.punctuation.replace(".", "")))
     
